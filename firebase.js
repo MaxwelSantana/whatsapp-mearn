aacd68af7caec4b97123fd2d1eddd277152487ee
Maxwel <maxwelsant96@icloud.com>
2021-10-04 18:03:35 -0300

    Initial Commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyB4ML5szb8fRVRZc_-juetT_FD0YSNafH8',
+    authDomain: 'whatsapp-mearn-811fc.firebaseapp.com',
+    projectId: 'whatsapp-mearn-811fc',
+    storageBucket: 'whatsapp-mearn-811fc.appspot.com',
+    messagingSenderId: '71860826501',
+    appId: '1:71860826501:web:9b02f9ad6a8a78113154ec',
+};
