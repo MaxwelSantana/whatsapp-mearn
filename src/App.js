8b56190c79f7364ce05c3cbcc6eeee6d5bf301bc
Maxwel <maxwelsant96@icloud.com>
2021-10-05 08:46:34 -0300

    front integration with pusher

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useEffect } from 'react';
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import Pusher from 'pusher-js';
 
 function App() {
+    useEffect(() => {
+        var pusher = new Pusher('1ef8341e29de91db578b', {
+            cluster: 'us2',
+        });
+
+        var channel = pusher.subscribe('messages');
+        channel.bind('inserted', function (data) {
+            alert(JSON.stringify(data));
+        });
+    }, []);
+
     return (
         <div className="app">
             <div className="app__body">
