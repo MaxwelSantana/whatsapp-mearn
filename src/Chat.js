875e86b5b58aaf3b7757fd6eb377217ad0f546e1
Maxwel <maxwelsant96@icloud.com>
2021-10-04 21:06:55 -0300

    chat styling

--- src/Chat.js
+++ src/Chat.js
@@ -1,5 +1,11 @@
 import { Avatar, IconButton } from '@material-ui/core';
-import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import {
+    AttachFile,
+    InsertEmoticon,
+    Mic,
+    MoreVert,
+    SearchOutlined,
+} from '@material-ui/icons';
 import React from 'react';
 import './Chat.css';
 
@@ -24,6 +30,38 @@ function Chat() {
                         <MoreVert />
                     </IconButton>
                 </div>
+            </div>
+            <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">Sonny</span>
+                    This is a message.
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+                <p className="chat__message chat__receiver">
+                    <span className="chat__name">Sonny</span>
+                    This is a message.
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+                <p className="chat__message">
+                    <span className="chat__name">Sonny</span>
+                    This is a message.
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+            </div>
+
+            <div className="chat__footer">
+                <InsertEmoticon />
+                <form>
+                    <input type="text" placeholder="Type a message" />
+                    <button type="submit">Send a message</button>
+                </form>
+                <Mic />
             </div>
         </div>
     );
